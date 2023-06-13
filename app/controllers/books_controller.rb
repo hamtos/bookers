177fa1a82241e563ae79a392edeb084c489324ac
hamtos <135288715+hamtos@users.noreply.github.com>
2023-06-13 12:14:07 +0000

    [pre-finish]beforeTest

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:success] = "Book was successfully created"
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book was successfully created"
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -15,16 +19,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:success] = "Book was successfully updated"
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully updated"
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
 
   def show
@@ -38,6 +45,6 @@ class BooksController < ApplicationController
   private
   # ストロングパラメータ
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.permit(:title,:body)
   end
 end
